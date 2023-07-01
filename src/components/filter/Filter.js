10f8b5677b613e5dfb23eabd63812e8eacbc5b3a
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-07-01 19:56:37 +0200

    commit 2

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 
 function Filter({ filterChange }) {
   // const contactList = useSelector(state => state.contacts);
