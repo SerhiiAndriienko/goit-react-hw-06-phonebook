aedc688cba3cd65a7fffa5b09c1b03b3209f8d7b
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-07-09 11:59:43 +0200

    ContactList change

--- src/components/contactList/ContactList.js
+++ src/components/contactList/ContactList.js
@@ -9,55 +9,50 @@ function ContactList({ onDeleteContact }) {
 
   return (
     <ul>
-      {(() => {
-        if (contactList[0] === undefined) {
-          return (
-            <span
-              style={{
-                fontStyle: 'italic',
-                textAlign: 'center',
-                display: 'block',
+      {!contactList[0] && (
+        <span
+          style={{
+            fontStyle: 'italic',
+            textAlign: 'center',
+            display: 'block',
+          }}
+        >
+          Your phonebook is empty
+        </span>
+      )}
+      {filterContacts === '' ??
+        contactList.map(contact => (
+          <li key={contact.id}>
+            <span>
+              {contact.name}: {contact.number}
+            </span>
+            <button
+              onClick={() => {
+                onDeleteContact(contact.id);
               }}
             >
-              Your phonebook is empty
+              Delete
+            </button>
+          </li>
+        ))}
+      {contactList
+        .filter(contact =>
+          contact.name.toLowerCase().includes(filterContacts.toLowerCase())
+        )
+        .map(contact => (
+          <li key={contact.id}>
+            <span>
+              {contact.name}: {contact.number}
             </span>
-          );
-        } else if (filterContacts === '') {
-          return contactList.map(contact => (
-            <li key={contact.id}>
-              <span>
-                {contact.name}: {contact.number}
-              </span>
-              <button
-                onClick={() => {
-                  onDeleteContact(contact.id);
-                }}
-              >
-                Delete
-              </button>
-            </li>
-          ));
-        } else {
-          return contactList
-            .filter(contact =>
-              contact.name.toLowerCase().includes(filterContacts.toLowerCase())
-            )
-            .map(contact => (
-              <li key={contact.id}>
-                <span>
-                  {contact.name}: {contact.number}
-                </span>
-                <button
-                  onClick={() => {
-                    onDeleteContact(contact.id);
-                  }}
-                >
-                  Delete
-                </button>
-              </li>
-            ));
-        }
-      })()}
+            <button
+              onClick={() => {
+                onDeleteContact(contact.id);
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
     </ul>
   );
 }
